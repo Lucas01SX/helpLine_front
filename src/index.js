50252bcade05ed659e5ca31d85aafe5b2a97f54c
Gustavo Teixeira de Melo <gmelo@plansul.com.br>
2025-01-09 16:01:37 -0300

    Corrigindo erro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';  // Alteração aqui
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Alteração aqui
+root.render(<App />);
