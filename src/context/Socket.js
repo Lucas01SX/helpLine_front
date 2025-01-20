6c0f9ae0357d256ece205114306ea6b32ee5ba43
Gustavo Teixeira de Melo <gmelo@plansul.com.br>
2025-01-20 11:04:51 -0300

    Fix direcionamento inicial

--- src/context/Socket.js
+++ src/context/Socket.js
@@ -1,9 +1,13 @@
 // socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://172.32.1.81', {
-    path: '/suporte-api/socket.io',
-    transports: ['polling'], // ou ['websocket'] se o WebSocket estiver funcionando
+// const socket = io('http://172.32.1.81', {
+//     path: '/suporte-api/socket.io',
+//     transports: ['polling'], // ou ['websocket'] se o WebSocket estiver funcionando
+// });
+const socket = io('http://172.32.1.81:9002', {
+    path: '/socket.io',
+    transports: ['polling'],
 });
 
 export default socket;
